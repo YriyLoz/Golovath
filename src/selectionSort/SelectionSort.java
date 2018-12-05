f91f1bc2408887f185b6fb5faf6ce20a4ee27219
YriyLoz <36788651+YriyLoz@users.noreply.github.com>
2018-12-05 19:34:17 +0300

    SelectionSort

--- src/selectionSort/SelectionSort.java
+++ src/selectionSort/SelectionSort.java
@@ -1,4 +1,24 @@
 package selectionSort;
 
+import java.util.Arrays;
+
 public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 2, 3, 5, 1, 4,};
+
+
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[min] > arr[j]) {
+                    min = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = tmp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
 }
